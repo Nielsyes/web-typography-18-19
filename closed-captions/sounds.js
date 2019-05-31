3f3419865894eb9e7cc01be8d2bec32648c23189
Niels <evniels95@gmail.com>
2019-05-31 23:47:13 +0200

    further changes for the up-to-30 sec part

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -7,10 +7,12 @@ sounds = [
 	12,
 	15.3,
 	19,
+	22,
 	23.2,
 	24.5,
+	29,
 	31,
-	32,
+	33,
 	34,
 	86
 ];
@@ -28,8 +30,25 @@ function addAndRemoveClass(soundClass, element, timeOut){
 	}, 10)
 }
 
+function addAndRemoveElement(soundClass, elementId, timeOut){
+	let element = document.getElementById(elementId);
+	let alreadySet;
+	setInterval(function(){
+		if (document.body.classList.contains(soundClass) && alreadySet != true) {
+			element.style.display = 'flex';
+			setTimeout(function() {
+				element.style.display = 'none';
+				alreadySet = true;
+			}, timeOut)
+		}
+	}, 10)
+}
+
 addAndRemoveClass('sound1', 'crt-overlay', 2000);
 addAndRemoveClass('sound3', 'gradient-overlay', 15000);
 addAndRemoveClass('sound4', 'alarm-gradient', 2000);
-addAndRemoveClass('sound7', 'alarm-gradient', 2000);
 addAndRemoveClass('sound8', 'alarm-gradient', 2000);
+addAndRemoveClass('sound10', 'alarm-gradient', 2000);
+
+addAndRemoveElement('sound4', 'bars', 9200);
+addAndRemoveElement('sound8', 'bars-big', 9000);
